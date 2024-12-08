0f6029828f8d2e864847ae786909967e11ebcae9
ramdhanrizkij <ramdhanrizkij@gmail.com>
2024-12-08 19:36:50 +0700

    Tambah fungsi nyala dan matikan relay

--- src/app/sensor/services/sensor.service.ts
+++ src/app/sensor/services/sensor.service.ts
@@ -118,62 +118,57 @@ export class SensorService {
         );
       }
 
-      // else if (sensorType === 'AMONIA') {
-      //   let amonia: number = parseFloat(msgJson.amonia);
-      //   if (amonia && amonia > 100) {
-      //     amonia = 0;
-      //   }
-      //   await this.prismaService.iotSensor.update({
-      //     where: {
-      //       id: iot?.id,
-      //     },
-      //     data: {
-      //       currentAmonia: amonia,
-      //     },
-      //   });
-      //   // {"device_id":"8KVP701731","air_quality":43,"nilaiRO":null,"amonia":"0.55"}
-      //   payload.sensorType = SensorType.GAS;
-      //   payload.value = amonia;
-      //   this.saveLogData(payload, SensorType.GAS).catch((e) =>
-      //     console.log('Failed to save log data : ', e.message),
-      //   );
-      // } else if (sensorType.includes('SUHU')) {
-      //   if (iot.tempThreshold && msgJson.temperature > iot.tempThreshold) {
-      //     this.mqtt.publish('d-farm/' + this.sensorId, 'RELAY2_ON');
-      //   } else {
-      //     this.mqtt.publish('d-farm/' + this.sensorId, 'RELAY2_OFF');
-      //   }
-      //   await this.prismaService.iotSensor.update({
-      //     where: {
-      //       id: iot?.id,
-      //     },
-      //     data: {
-      //       currentTemperature: msgJson.temperature ?? 0,
-      //     },
-      //   });
-      //   payload.sensorType = SensorType.TEMP;
-      //   payload.value = msgJson.temperature;
-      //   this.saveLogData(payload, SensorType.TEMP).catch((e) =>
-      //     console.log('Failed to save log data : ', e.message),
-      //   );
-  
-      //   await this.prismaService.iotSensor.update({
-      //     where: {
-      //       id: iot?.id,
-      //     },
-      //     data: {
-      //       currentHumidty: msgJson.humidity ?? 0,
-      //     },
-      //   });
-      //   payload.sensorType = SensorType.HUMIDITY;
-      //   payload.value = msgJson.humidity;
-      //   this.saveLogData(payload, SensorType.HUMIDITY).catch((e) =>
-      //     console.log('Failed to save log data : ', e.message),
-      //   );
-      // }
+      this.checkRelay().catch((e)=>{
+        console.log("Failed to check relay data : ",e)
+      })
     }
 
    
+  }
+
+  async checkRelay(){
+    const devices = await this.prismaService.iotSensor.findMany({where:{deletedAt:null}})
+    for(let device of devices){
+      const result = await this.prismaService.sensorDevice.groupBy({
+        by: ['type'], 
+        where: {
+          deviceId: device.id, 
+        },
+        _max: {
+          lastestValue: true, 
+        },
+      });
+      
+      // Transform the result to match the desired format
+      const transformedResult = result.map((item) => ({
+        type: item.type,
+        value: item._max.lastestValue,
+      }));
+      
+      const ldrEntry = transformedResult.find((item) => item.type === 'LDR');
+      const amoniaEntry = transformedResult.find((item) => item.type === 'GAS');
+      const tempEntry = transformedResult.find((item) => item.type === 'TEMP');
+      const humidityEntry = transformedResult.find((item) => item.type === 'HUMIDITY');
+
+      if (ldrEntry && ldrEntry.value! > 500) {
+        this.mqtt.publish('d-farm/' + this.sensorId, 'RELAY1_OFF');
+      }else{
+          this.mqtt.publish('d-farm/' + this.sensorId, 'RELAY1_ON');
+      }
+      setTimeout(()=>{
+
+      },1000)
+      const amoniaValue = amoniaEntry?amoniaEntry.value:0;
+      const tempValue = tempEntry?tempEntry.value:0;
+      const humiValue = humidityEntry?humidityEntry.value:0;
+
+      if((amoniaValue!>device.amoniaThreshold!) || (tempValue! > device.tempThreshold) || (humiValue!> device.humidityThreshold)){
+        this.mqtt.publish('d-farm/' + this.sensorId, 'RELAY2_ON');
+      }else{
+        this.mqtt.publish('d-farm/' + this.sensorId, 'RELAY2_OFF');
+      }
+
+    }
   }
 
   saveLogBasedOnTopic(message: string, topic: string) {
