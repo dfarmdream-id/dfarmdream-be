c4f4376464c8aa68ba66cdc0b206656ee86f8d51
antowirantoIO <antowrnto11@gmail.com>
2025-01-05 13:54:28 +0700

    fix for jurnal and adjust temnplate error

--- src/app/biaya/services/biaya.service.ts
+++ src/app/biaya/services/biaya.service.ts
@@ -46,169 +46,159 @@ export class BiayaService {
     const qtyOut = Number(payload.qty ?? '0');
 
     return from(
-      this.prismaService.persediaanPakanObat.findFirst({
-        where: { id: payload.persediaanBarangId ?? '' },
-        include: { goods: true },
-      }),
+      this.journalTemplatesService.findFirstByJournalTypeId(
+        payload.journalTypeId,
+      ),
     ).pipe(
-      switchMap((barang) => {
-        if (barang) {
-          const qtyAkhir = barang.qty - qtyOut;
-          const totalHarga = barang.harga * qtyOut;
+      switchMap((journalTemplate) => {
+        if (!journalTemplate)
+          return throwError(() => new Error('Journal template not found'));
 
-          return from(
-            this.prismaService.kartuStokBarang
-              .create({
-                data: {
-                  tanggal: currentDate,
-                  barangId: barang.id,
-                  cageId: payload.cageId,
-                  siteId: payload.siteId,
-                  batchId: payload.batchId,
-                  qtyAsal: barang.qty,
-                  qtyIn: 0,
-                  qtyOut,
-                  qtyAkhir,
-                  harga: barang.harga,
-                  total: totalHarga,
-                  karyawanId: payload.userId,
-                  keterangan: payload.keterangan,
-                  status: 0,
-                },
-              })
-              .then(() =>
-                this.prismaService.persediaanPakanObat.update({
-                  where: { id: barang.id },
-                  data: { qty: qtyAkhir },
-                }),
-              ),
-          ).pipe(switchMap(() => from(Promise.resolve(barang))));
-        }
-        return from(Promise.resolve(null));
-      }),
-      switchMap((barang) =>
-        from(
-          this.prismaService.batch.findUnique({
-            where: { id: payload.batchId },
+        return from(
+          this.prismaService.persediaanPakanObat.findFirst({
+            where: { id: payload.persediaanBarangId ?? '' },
+            include: { goods: true },
           }),
         ).pipe(
-          switchMap((batch) =>
-            from(
-              this.prismaService.biaya.create({
-                data: {
-                  tanggal: payload.tanggal,
-                  kategoriBiaya: { connect: { id: payload.kategoriId } },
-                  cage: { connect: { id: payload.cageId } },
-                  site: { connect: { id: payload.siteId } },
-                  ...(payload.persediaanBarangId && {
-                    persediaanPakanObat: {
-                      connect: { id: payload.persediaanBarangId },
-                    },
-                  }),
-                  user: { connect: { id: payload.userId } },
-                  qtyOut,
-                  batch: {
-                    connect: { id: payload.batchId },
-                  },
-                  biaya: payload.biaya ?? 0,
-                  status: payload.status,
-                  keterangan: payload.keterangan,
-                },
+          switchMap((barang) => {
+            if (!barang)
+              return throwError(() => new Error('Inventory not found'));
+
+            const qtyAkhir = barang.qty - qtyOut;
+            const totalHarga = barang.harga * qtyOut;
+
+            return from(
+              this.prismaService.batch.findUnique({
+                where: { id: payload.batchId },
               }),
             ).pipe(
-              switchMap((saved) =>
-                from(
-                  this.journalTemplatesService.findFirstByJournalTypeId(
-                    payload.journalTypeId,
-                  ),
-                ).pipe(
-                  switchMap((journalTemplate) => {
-                    if (!journalTemplate) return from(Promise.resolve(null));
+              switchMap((batch) => {
+                const totalBiaya = payload.biaya ?? 0;
+                const typeGood = barang.goods?.type?.toLowerCase() ?? 'pakan';
 
-                    const totalBiaya = payload.biaya ?? 0;
-                    const typeGood =
-                      barang?.goods?.type?.toLowerCase() ?? 'pakan';
+                const ledgerCounts =
+                  journalTemplate.journalTemplateDetails.reduce(
+                    (acc, detail) => {
+                      acc[detail.typeLedger] =
+                        (acc[detail.typeLedger] || 0) + 1;
+                      return acc;
+                    },
+                    {} as Record<string, number>,
+                  );
 
-                    const ledgerCounts =
-                      journalTemplate.journalTemplateDetails.reduce(
-                        (acc, detail) => {
-                          acc[detail.typeLedger] =
-                            (acc[detail.typeLedger] || 0) + 1;
-                          return acc;
-                        },
-                        {} as Record<string, number>,
-                      );
+                const details: CreateJournalDetailDto[] =
+                  journalTemplate.journalTemplateDetails.map((detail) => {
+                    const isRelevant = detail.coa.name
+                      .toLowerCase()
+                      .includes(typeGood);
+                    const nominal =
+                      ledgerCounts[detail.typeLedger] === 1
+                        ? totalBiaya
+                        : ledgerCounts[detail.typeLedger] > 1 && isRelevant
+                          ? totalBiaya
+                          : 0;
 
-                    const details: CreateJournalDetailDto[] =
-                      journalTemplate.journalTemplateDetails.map((detail) => {
-                        const isRelevant = detail.coa.name
-                          .toLowerCase()
-                          .includes(typeGood);
-
-                        const nominal =
-                          ledgerCounts[detail.typeLedger] === 1
-                            ? totalBiaya
-                            : ledgerCounts[detail.typeLedger] > 1 && isRelevant
-                              ? totalBiaya
-                              : 0;
-
-                        return {
-                          coaCode: detail.coa.code,
-                          debit: detail.typeLedger === 'DEBIT' ? nominal : 0,
-                          credit: detail.typeLedger === 'CREDIT' ? nominal : 0,
-                          note: `
-                            Biaya: ${
-                              barang?.goods?.name ?? 'Lainnya'
-                            } (Batch: ${batch?.name ?? 'Tidak Diketahui'}) - ${detail.coa.name} - ${payload.keterangan}
-                          `,
-                        };
-                      });
-
-                    const journalDto: CreateJournalDto = {
-                      code: `JN-${DateTime.now().toFormat('yy-MM')}-$${
-                        Math.floor(Math.random() * 1000) + 1
-                      }`,
-                      date: payload.tanggal,
-                      debtTotal: details.reduce(
-                        (acc, curr) => acc + curr.debit,
-                        0,
-                      ),
-                      creditTotal: details.reduce(
-                        (acc, curr) => acc + curr.credit,
-                        0,
-                      ),
-                      batchId: payload.batchId,
-                      status: '1',
-                      journalTypeId: payload.journalTypeId,
-                      cageId: payload.cageId,
-                      siteId: payload.siteId,
-                      details,
+                    return {
+                      coaCode: detail.coa.code,
+                      debit: detail.typeLedger === 'DEBIT' ? nominal : 0,
+                      credit: detail.typeLedger === 'CREDIT' ? nominal : 0,
+                      note: `
+                      Biaya: ${barang.goods?.name ?? 'Lainnya'} (Batch: ${batch?.name ?? 'Tidak Diketahui'}) - ${detail.coa.name} - ${payload.keterangan}
+                    `,
                     };
+                  });
 
-                    return this.journalService.create(
-                      journalDto,
-                      payload.userId,
-                    );
-                  }),
-                  switchMap((journal) => {
-                    if (!journal) {
-                      return throwError(
-                        () => new Error('Failed to create journal entry'),
+                const journalDto: CreateJournalDto = {
+                  code: `JN-${DateTime.now().toFormat('yy-MM')}-$$${Math.floor(Math.random() * 1000) + 1}`,
+                  date: payload.tanggal,
+                  debtTotal: details.reduce((acc, curr) => acc + curr.debit, 0),
+                  creditTotal: details.reduce(
+                    (acc, curr) => acc + curr.credit,
+                    0,
+                  ),
+                  batchId: payload.batchId,
+                  status: '1',
+                  journalTypeId: payload.journalTypeId,
+                  cageId: payload.cageId,
+                  siteId: payload.siteId,
+                  details,
+                };
+
+                return this.journalService
+                  .create(journalDto, payload.userId)
+                  .pipe(
+                    switchMap((journal) => {
+                      if (!journal)
+                        return throwError(
+                          () => new Error('Failed to create journal entry'),
+                        );
+
+                      return from(
+                        this.prismaService.kartuStokBarang
+                          .create({
+                            data: {
+                              tanggal: currentDate,
+                              barangId: barang.id,
+                              cageId: payload.cageId,
+                              siteId: payload.siteId,
+                              batchId: payload.batchId,
+                              qtyAsal: barang.qty,
+                              qtyIn: 0,
+                              qtyOut,
+                              qtyAkhir,
+                              harga: barang.harga,
+                              total: totalHarga,
+                              karyawanId: payload.userId,
+                              keterangan: payload.keterangan,
+                              status: 0,
+                            },
+                          })
+                          .then(() =>
+                            this.prismaService.persediaanPakanObat.update({
+                              where: { id: barang.id },
+                              data: { qty: qtyAkhir },
+                            }),
+                          ),
+                      ).pipe(
+                        switchMap(() =>
+                          this.prismaService.biaya.create({
+                            data: {
+                              tanggal: payload.tanggal,
+                              kategoriBiaya: {
+                                connect: { id: payload.kategoriId },
+                              },
+                              cage: { connect: { id: payload.cageId } },
+                              site: { connect: { id: payload.siteId } },
+                              ...(payload.persediaanBarangId && {
+                                persediaanPakanObat: {
+                                  connect: { id: payload.persediaanBarangId },
+                                },
+                              }),
+                              user: { connect: { id: payload.userId } },
+                              qtyOut,
+                              batch: { connect: { id: payload.batchId } },
+                              biaya: payload.biaya ?? 0,
+                              status: payload.status,
+                              keterangan: payload.keterangan,
+                            },
+                          }),
+                        ),
                       );
-                    }
-                    return from(Promise.resolve(saved));
-                  }),
-                ),
-              ),
-            ),
-          ),
-        ),
-      ),
+                    }),
+                  );
+              }),
+            );
+          }),
+        );
+      }),
       catchError((err) => {
         console.error('Error while creating biaya:', err.message);
         return throwError(
           () =>
-            new HttpException('Failed to create biaya', HttpStatus.BAD_REQUEST),
+            new HttpException(
+              'Gagal membuat biaya, ' + err.message,
+              HttpStatus.BAD_REQUEST,
+            ),
         );
       }),
     );
